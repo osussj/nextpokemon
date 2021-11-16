b84792184480e1915853a1a4a61b0ab7032c508c
osu <joanosuna9@gmail.com>
2021-11-16 20:10:58 +0100

    Added navigation in ssg

---
+++ pages/mypokemons/ssg/[id].js
@@ -0,0 +1,37 @@
+import { useRouter } from "next/router";
+
+const MyPokemons = ({ pokemon }) => {
+  const router = useRouter();
+  if (router.isFallback) {
+    return <h2>LOADING</h2>;
+  }
+  return (
+    <>
+      <h1>MyPokemons ssg</h1>
+      <h2>{pokemon.name}</h2>
+    </>
+  );
+};
+
+export const getStaticPaths = async () => {
+  const response = await fetch("https://pokedex-carlos.herokuapp.com/pokemon");
+  const pokemons = await response.json();
+  const paths = pokemons.map((pokemon) => ({
+    params: { id: "" + pokemon.id },
+  }));
+  return { paths, fallback: true };
+};
+
+export const getStaticProps = async ({ params: { id } }) => {
+  const response = await fetch(
+    "https://pokedex-carlos.herokuapp.com/pokemon/" + id
+  );
+  const pokemon = await response.json();
+  return {
+    props: {
+      pokemon: pokemon,
+    },
+    revalidate: 30,
+  };
+};
+export default MyPokemons;
--- pages/mypokemons/ssg/index.js
+++ pages/mypokemons/ssg/index.js
@@ -1,5 +1,28 @@
-const MyPokemons = () => {
-  return <h1>MyPokemons ssg</h1>;
+import Link from "next/link";
+
+const MyPokemons = ({ pokemons }) => {
+  return (
+    <>
+      <h1>MyPokemons ssg</h1>
+      {pokemons &&
+        pokemons.map((pokemon) => (
+          <div key={pokemon.id}>
+            <Link href={`/mypokemons/ssg/${pokemon.id}`}>
+              <a>{pokemon.name}</a>
+            </Link>
+          </div>
+        ))}
+    </>
+  );
 };
 
+export const getServerSideProps = async () => {
+  const response = await fetch("https://pokedex-carlos.herokuapp.com/pokemon");
+  const pokemons = await response.json();
+  return {
+    props: {
+      pokemons: pokemons,
+    },
+  };
+};
 export default MyPokemons;
